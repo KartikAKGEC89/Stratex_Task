39cf01497b452aa554b41f352fc4df66f67685e7
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-05-29 15:49:01 +0530

    FEAT: CSV FILE UPLOAD IN DATABASE

--- index.js
+++ index.js
@@ -6,10 +6,50 @@ import db from "./config/Database.js";
 import router from "./routes/index.js";
 dotenv.config();
 const app = express();
+import csvtojson from "csvtojson";
+import multer from "multer";
+import path from "path";
+import BookDetail from './models/CsvModel.js';
+
+
+const upload = multer({ dest: 'uploads/' });
  
 app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
+
+
+app.post('/upload', upload.single('csvfile'), async (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+
+    const filePath = req.file.path;
+
+    try {
+        const jsonArray = await csvtojson().fromFile(filePath);
+
+        // Map each row in the CSV file to a new instance of the BookDetail model
+        await Promise.all(jsonArray.map(async (row, index) => {
+            try {
+                await BookDetail.create({
+                    title: row.title,
+                    author: row.author,
+                    publishedDate: row.publishedDate,
+                    price: row.price
+                });
+                console.log(`Item at row ${index + 1} inserted successfully`);
+            } catch (error) {
+                console.error(`Unable to insert item at row ${index + 1}:`, error);
+            }
+        }));
+
+        res.send('File uploaded and data inserted successfully');
+    } catch (error) {
+        console.error('Error processing file:', error);
+        res.status(500).send('Error processing file');
+    }
+});
  
 app.listen(5000, ()=> console.log('Server running at port 5000'));
---
+++ models/CsvModel.js
@@ -0,0 +1,25 @@
+// models/BookDetail.js
+
+import { DataTypes } from 'sequelize';
+import db from '../config/Database.js';
+
+const BookDetail = db.define('bookdetails', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    publishedDate: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    }
+});
+
+export default BookDetail;
