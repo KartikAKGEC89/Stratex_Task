d655a67a13e64afd21823c3108cb96e66cd19d0d
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-05-30 13:02:06 +0530

    FEAT: FIX AUTHENTICATION

--- middleware/VerifyToken.js
+++ middleware/VerifyToken.js
@@ -6,7 +6,7 @@ export const verifyToken = (req, res, next) => {
     if(token == null) return res.sendStatus(401);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err) return res.sendStatus(403);
-        req.email = decoded.email;
+        req.user = decoded;
         next();
     })
 }
